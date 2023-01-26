53b946a112792a687d4ab49b7ffe5bdfcf555a9b
Adam Harris <afharris87@gmail.com>
2023-01-26 15:22:35 -0600

    working on more_exercises

--- more_exercises.sql
+++ more_exercises.sql
@@ -98,3 +98,15 @@ SELECT * FROM film ORDER BY length;
 
 -- 8b Select all distinct ratings from the film table ordered by rating in descending order.
 SELECT DISTINCT rating FROM film ORDER BY rating DESC;
+
+-- 8c Select the payment date and amount columns from the payment table for the first 20 payments ordered by payment amount in descending order.
+SELECT payment_date, amount FROM payment 
+	ORDER BY amount DESC LIMIT 20;
+	
+-- 8d Select the title, description, special features, length, and rental duration columns from the film table for the first 10 films with behind the scenes footage under 2 hours in length and a rental duration between 5 and 7 days, ordered by length in descending order.
+DESCRIBE film;
+SELECT special_features FROM film LIMIT 5;
+SELECT title, description, special_features, length, rental_duration FROM film 
+	WHERE (special_features LIKE '%Behind the Scenes%') AND (length < 120)
+	AND (rental_duration BETWEEN 5 AND 7)
+	ORDER BY length DESC;
