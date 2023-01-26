78a0b77c3218b928b56bb53dc53f8ef9916b9f25
Adam Harris <afharris87@gmail.com>
2023-01-26 14:46:33 -0600

    add limit_exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,41 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles ORDER BY title DESC LIMIT 10;
+/* 2 List the first 10 distinct last name sorted in descending order.
+Technique Leader
+Staff
+Senior Staff
+Senior Engineer
+Manager
+Engineer
+Assistant Engineer */
+
+SELECT first_name, last_name FROM employees 
+	WHERE (hire_date LIKE '199%') 
+		AND (birth_date LIKE '%12-25')
+	ORDER BY hire_date
+	LIMIT 5;
+/* 3 Write a comment in your code that lists the five names of the employees returned.
+Alselm	Cappello
+Utz	Mandell
+Bouchung	Schreiter
+Baocai	Kushner
+Petter	Stroustrup */
+
+SELECT first_name, last_name FROM employees 
+	WHERE (hire_date LIKE '199%') 
+		AND (birth_date LIKE '%12-25')
+	ORDER BY hire_date
+	LIMIT 5
+	OFFSET 45;
+/* 4 Update the query to find the tenth page of results.
+Pranay	Narwekar
+Marjo	Farrow
+Ennio	Karcich
+Dines	Lubachevsky
+Ipke	Fontan 
+
+What is the relationship between OFFSET (number of results to skip), 
+LIMIT (number of results per page), and the page number? 
+The offset will be the desired (page number - 1) * (the limit)
+Offset = (page_num-1) * Limit  */
